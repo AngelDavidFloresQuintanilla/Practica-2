0e2143a9c698e7fc7e5a231278cd76c1f728cd8f
x1 <x1@DESKTOP-MAD90RV>
2023-10-24 10:34:00 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Practica_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Hello, " + textBox1.Text + "!";
+        }
+    }
+}
